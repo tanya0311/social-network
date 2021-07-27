9211ee1a18154e1fcc69a6ab114ae3cacb702533
tanya0311 <tanya03.11@mail.ru>
2021-07-27 16:16:07 +0300

    83 reselect

--- src/redux/users-selectors.ts
+++ src/redux/users-selectors.ts
@@ -1,21 +1,41 @@
-import { RootReducersType } from "./redux-store"
+import { createSelector, Selector } from "reselect";
+import { RootReducersType } from "./redux-store";
+import { UsersPropsType } from "./users-reducer";
 
-export const GetUsers=(state:RootReducersType)=>{
-   return state.userPage.users
-}
-export const GetPageSize=(state:RootReducersType)=>{
-   return state.userPage.pageSize
-}
-export const GetTotalUsersCount=(state:RootReducersType)=>{
-   return  state.userPage.totalUsersCount
-}
-export const GetCurrentPage=(state:RootReducersType)=>{
-   return   state.userPage.currentPage
-}
-export const GetIsFetching =(state:RootReducersType)=>{
-   return   state.userPage.isFetching
-}
-export const GetFollowingInProgress =(state:RootReducersType)=>{
-   return    state.userPage.followingInProgress
-}
+export const GetUsersPrimitive: Selector<
+  RootReducersType,
+  Array<UsersPropsType>
+> = (state) => {
+  return state.userPage.users;
+};
+// export const GetUsersSelector=(state:RootReducersType)=>{
+//    return GetUsersPrimitive(state).filter(u=> true)
+// }
 
+export const GetUsers = createSelector(
+  GetUsersPrimitive,
+  (users: Array<UsersPropsType>) => {
+    return users.filter((u) => true);
+  }
+);
+
+export const GetPageSize: Selector<RootReducersType, number> = (state) => {
+  return state.userPage.pageSize;
+};
+export const GetTotalUsersCount: Selector<RootReducersType, number> = (
+  state
+) => {
+  return state.userPage.totalUsersCount;
+};
+export const GetCurrentPage: Selector<RootReducersType, number> = (state) => {
+  return state.userPage.currentPage;
+};
+export const GetIsFetching: Selector<RootReducersType, boolean> = (state) => {
+  return state.userPage.isFetching;
+};
+export const GetFollowingInProgress: Selector<
+  RootReducersType,
+  Array<number>
+> = (state) => {
+  return state.userPage.followingInProgress;
+};
